388039ee870cbda6536950b5dcda9fed653f96b4
lailazakaria <lailaza229@gmail.com>
2023-08-16 19:36:59 +0300

    add profile

--- src/app/profile/components/profile/profile.component.ts
+++ src/app/profile/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  personalInfoForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.personalInfoForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      mobileNumber: ['', [Validators.required, Validators.pattern('^09[0-9]{8}$')]]
+    });
+  }
+
+  saveChanges() {
+    if (this.personalInfoForm.valid) {
+
+      console.log(this.personalInfoForm.value);
+    }
+  }
 
 }
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './components/profile/profile.component';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     ProfileComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule, MatInputModule
+  ],
+  exports: [
+    ProfileComponent
   ]
 })
 export class ProfileModule { }
